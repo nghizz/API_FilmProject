eabb02ead9839b9643521ec3b8c9b37a3d6261d6
TQH0701 <huytran.100403@gmail.com>
2024-11-20 23:52:24 +0700

    update login, register

---
+++ API_Film/Models/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Film.Models
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
--- API_Film/Models/User.cs
+++ API_Film/Models/User.cs
@@ -8,7 +8,7 @@
         public string Role { get; set; }
 
         // Navigation Properties không cần thiết khi đăng nhập
-        public ICollection<Order> Orders { get; set; }
-        public ICollection<MovieReview> MovieReviews { get; set; }
+        public ICollection<Order> Orders { get; set; } = new List<Order>();  // Khởi tạo danh sách mặc định
+        public ICollection<MovieReview> MovieReviews { get; set; } = new List<MovieReview>();  // Khởi tạo danh sách mặc định
     }
 }
