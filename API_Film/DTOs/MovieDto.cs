bc7044268e7d55a2d9ba4b617ff198fda7e1e877
HieuKO <nguyenngochieu02022003email@gmail.com>
2024-12-04 21:02:10 +0700

    update Hieu

---
+++ API_Film/DTOs/MovieDto.cs
@@ -0,0 +1,14 @@
+﻿namespace API_Film.DTOs
+{
+    public class MovieDto
+    {
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public string Genre { get; set; }
+            public int Duration { get; set; }
+            public string Description { get; set; }
+            public string Director { get; set; }
+            public string ImageUrl { get; set; }
+    }
+}
+
---
+++ API_Film/DTOs/PromotionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace API_Film.DTOs
+{
+    public class PromotionDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Discount { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double MinimumAmount { get; set; }
+    }
+}
